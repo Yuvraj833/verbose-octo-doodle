b19b8c56ddc9502e4de951142f42a7e038ff0e6b
Yuvraj833 <124455751+Yuvraj833@users.noreply.github.com>
2023-04-28 09:36:52 +0530

    Create x.py

---
+++ x.py
@@ -0,0 +1,177 @@
+To stream a specific video and sound on YouTube, you can use the YouTube Live Streaming API in combination with FFmpeg. Here's an example Python code that uses these tools to stream a video file and audio file to YouTube:
+
+```python
+
+import subprocess
+
+from google.oauth2.credentials import Credentials
+
+from googleapiclient.discovery import build
+
+from googleapiclient.errors import HttpError
+
+# Set the path to the video and audio files you want to stream
+
+VIDEO_PATH = '/path/to/video.mp4'
+
+AUDIO_PATH = '/path/to/audio.mp3'
+
+# Set the credentials for the YouTube API
+
+creds = Credentials.from_authorized_user_file('/path/to/credentials.json', ['https://www.googleapis.com/auth/youtube.force-ssl'])
+
+youtube = build('youtube', 'v3', credentials=creds)
+
+# Get the YouTube channel ID
+
+channel_response = youtube.channels().list(
+
+    part='snippet',
+
+    mine=True
+
+).execute()
+
+channel_id = channel_response['items'][0]['id']
+
+# Create a new live stream
+
+try:
+
+    stream_response = youtube.liveStreams().insert(
+
+        part='snippet,cdn',
+
+        body={
+
+            'snippet': {
+
+                'title': 'My live stream',
+
+                'description': 'This is a live stream created using the YouTube Data API'
+
+            },
+
+            'cdn': {
+
+                'ingestionType': 'rtmp',
+
+                'resolution': '720p',
+
+                'frameRate': '30fps'
+
+            }
+
+        }
+
+    ).execute()
+
+except HttpError as e:
+
+    print(f'An error occurred: {e}')
+
+    exit()
+
+# Get the stream URL and key
+
+ingestion_info = stream_response['cdn']['ingestionInfo']
+
+stream_url = ingestion_info['ingestionAddress'] + '/' + ingestion_info['streamName']
+
+stream_key = ingestion_info['streamName']
+
+# Set the FFmpeg command to stream the video and audio files to YouTube
+
+ffmpeg_command = f'ffmpeg -re -i "{VIDEO_PATH}" -i "{AUDIO_PATH}" -c copy -f flv "{stream_url}"'
+
+# Start the FFmpeg process
+
+ffmpeg_process = subprocess.Popen(ffmpeg_command, shell=True)
+
+# Bind the live stream to the YouTube broadcast
+
+try:
+
+    broadcast_response = youtube.liveBroadcasts().insert(
+
+        part='snippet,status,contentDetails',
+
+        body={
+
+            'snippet': {
+
+                'title': 'My live stream',
+
+                'description': 'This is a live stream created using the YouTube Data API',
+
+                'scheduledStartTime': '2023-04-30T00:00:00.000Z',
+
+                'scheduledEndTime': '2023-04-30T01:00:00.000Z',
+
+                'channelId': channel_id,
+
+                'thumbnails': {
+
+                    'default': {
+
+                        'url': 'http://example.com/thumbnail.jpg',
+
+                        'width': 120,
+
+                        'height': 90
+
+                    }
+
+                }
+
+            },
+
+            'status': {
+
+                'privacyStatus': 'public',
+
+                'selfDeclaredMadeForKids': False
+
+            },
+
+            'contentDetails': {
+
+                'monitorStream': {
+
+                    'enableMonitorStream': True,
+
+                    'broadcastStreamDelayMs': 0,
+
+                    'embedHtml': 'Sample embed HTML'
+
+                },
+
+                'enableLowLatency': True,
+
+                'enableAutoStart': True
+
+            }
+
+        }
+
+    ).execute()
+
+except HttpError as e:
+
+    print(f'An error occurred: {e}')
+
+    ffmpeg_process.kill()
+
+    exit()
+
+# Print the broadcast URL
+
+broadcast_id = broadcast_response['id']
+
+broadcast_url = f'https://www.youtube.com/watch?v={broadcast_id}'
+
+print(f'Broadcast URL: {broadcast_url}')
+
+# Stop the FFmpeg process
+
+ffmpeg_process
